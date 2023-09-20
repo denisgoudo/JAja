7412deb0ebdbb3d0b8f5dcd365d34b450a047dba
denisgoudo <125676670+denisgoudo@users.noreply.github.com>
2023-09-20 11:32:46 -0300

    Add files via upload

---
+++ JAja-main/main.js
@@ -0,0 +1,20 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();   
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom('#som_tacla_pom');
+    }
+
+    contador = contador + 1;
+
+    console.log(contador);
+
+}
